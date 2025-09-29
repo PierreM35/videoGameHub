d9c2d4b991720fe5c113b61460264640aa6d3526
menardp21 <menardp21@gmail.com>
2025-09-29 22:46:45 +0200

    clean gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,26 +13,25 @@ function GameGrid({ gameQuery }: Props) {
     const { data, error, isLoading } = useGames(gameQuery);
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+    if (error) return <p>{error}</p>;
+
     return (
-        <>
-            {error && <p>{error}</p>}
-            <Stack 
-                direction="row" 
-                spacing={3} 
-                useFlexGap
-                sx={{ flexWrap: 'wrap' }}>
-                    {isLoading && skeletons.map(skeleton => (
-                        <GameCardContainer key={skeleton} >
-                            <GameCardSkeleton />
-                        </GameCardContainer>
-                    ))}
-                    {data.map(game => (
-                        <GameCardContainer key={game.id} >
-                            <GameCard game={game} />
-                        </GameCardContainer>
-                    ))}
-            </Stack>
-        </>
+        <Stack 
+            direction="row" 
+            spacing={3} 
+            useFlexGap
+            sx={{ flexWrap: 'wrap' }}>
+                {isLoading && skeletons.map(skeleton => (
+                    <GameCardContainer key={skeleton} >
+                        <GameCardSkeleton />
+                    </GameCardContainer>
+                ))}
+                {data.map(game => (
+                    <GameCardContainer key={game.id} >
+                        <GameCard game={game} />
+                    </GameCardContainer>
+                ))}
+        </Stack>
     );
 };
 
