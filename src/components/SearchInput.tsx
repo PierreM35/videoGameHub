15bf14d1e7f728166a4a3187e7435ebae0b55901
menardp21 <menardp21@gmail.com>
2025-10-19 09:47:23 +0200

    add navigate home to home btn

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,17 +2,19 @@ import { InputAdornment, TextField, type TextFieldProps } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useRef } from "react";
 import useGameQueryStore from "../stateManagement/store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInputs = () => {
-  
     const searchRef = useRef<TextFieldProps>(null);
     const setSearchText = useGameQueryStore(s => s.setSearchText);
+    const navigate = useNavigate();
   
     return (
         <form onSubmit={e => {
             e.preventDefault();
             if (searchRef.current)
                 setSearchText(searchRef.current.value as string);
+                navigate("/");
             }}>
             <TextField 
                 inputRef={searchRef}
